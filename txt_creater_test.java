3bd7e06e6cd79bf58ec9a4a26d8a920ceb971b6f
EthanKamYC <yincheungkam@gmail.com>
2026-03-27 10:27:21 +0800

    Implement AnotherTestExample for file writing
    Added AnotherTestExample class to write information to a file.

--- txt_creater_test.java
+++ txt_creater_test.java
@@ -15,3 +15,36 @@ public class BufferedWriterExample {
         }
     }
 }
+
+public class AnotherTestExample {
+    public static void main(String page_title, String URL, String last_mod_date, String size_of_page, int keyword_count, 
+                            String[] keywords, int[] freq, int link_count, String[] child_links) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("debug_for_test_code.txt"))) {
+            bw.write(page_title);
+            bw.newLine();
+            bw.write(URL);
+            bw.newLine();
+            bw.write(last_mod_date);
+            bw.write(", ");
+            bw.write(size_of_page);
+            bw.newLine();
+            for (int i = 0, i < keyword_count; i++)
+            {
+                bw.write(keywords[i]);
+                bw.write(" ");
+                bw.write(freq[i]);
+                if (i + 1 != keyword_count)
+                {
+                    bw.write(";");
+                }    
+            }    
+            for (int i = 0; i < link_count; i++)
+            {
+                bw.write(child_links[i]);
+                bw.newLine();
+            }        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
