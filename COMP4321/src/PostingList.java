a60e586d4df1c0b54d9b0376abd1b22c1b01ab01
sean111048 <sean111048@gmail.com>
2026-03-29 12:17:06 +0800

    Update PostingList.java

--- COMP4321/src/PostingList.java
+++ COMP4321/src/PostingList.java
@@ -3,18 +3,26 @@ import java.util.*;
 
 public class PostingList implements Serializable {
     private static final long serialVersionUID = 1L;
-    private Map<String, Integer> postings; // docId -> tf
+    private Map<String, List<Integer>> postings; // docId -> tf
 
     public PostingList() {
         postings = new HashMap<>();
     }
 
-    public void addOccurrence(String docId) {
-        postings.put(docId, postings.getOrDefault(docId, 0) + 1);
+    public void addOccurrence(String docId, int position) {
+        postings.computeIfAbsent(docId, k -> new ArrayList<>()).add(position);
     }
 
-    public int getTermFrequency(String docId) {
+    public List<Integer> getPositions(String docId) {
+        return postings.getOrDefault(docId, Collections.emptyList());
+    }
+
+    /*public int getTermFrequency(String docId) {
         return postings.getOrDefault(docId, 0);
+    }*/
+    
+    public Set<String> getDocuments() {
+        return postings.keySet();
     }
 
     public int getDocumentFrequency() {
